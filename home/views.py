ad244e5d8ff4a0afec590d447e7b52fb3e68a033
Hao Jun Chen <100190491+RayHao2@users.noreply.github.com>
2022-08-15 12:50:53 -0700

    fixing jquery

--- home/views.py
+++ home/views.py
@@ -60,7 +60,7 @@ def submit(request):
         # getting ip address
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
-            ip = x_forwarded_for.spli(',')[0]
+            ip = x_forwarded_for.split(',')[0]
         else:
             ip = request.META.get('REMOTE_ADDR')
         print("=== ip:" + ip)
